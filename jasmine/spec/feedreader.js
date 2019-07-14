67bd4f412303ac0c1639ca11b121a955a509eab6
Edwin Ang <edwinangdesign@gmail.com>
2019-07-14 13:08:11 +1000

    test to ensure that at least one entry exist in the DOM when the feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -92,14 +92,27 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+        beforeEach(function(done){
+            // load feed 0, Udacity Blog
+            loadFeed(0, function(){
+                done();
+            });
+        });
+        
+        it('should have at least a single entry', function(done) {
+            // ensure that feed entries has been populated in the DOM
+            expect($('.feed .entry').length).toBeGreaterThanOrEqual(1);
+            done();
+        });
+    });
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
